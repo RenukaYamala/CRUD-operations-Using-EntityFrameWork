eb195ec4bbe88098f99abb3feb4ae26897edada4
RenukaYamala <renuka.yamala@neudesic.com>
2018-06-08 09:31:57 +0530

    First time commits

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Employee_UsingEntityFrameWork.Data;
+using Employee_UsingEntityFrameWork.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_UsingEntityFrameWork.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Employee")]
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (var _context = new EmployeeDBContext())
+            {
+                var listOfStudents = new List<EmployeeModel>();
+                listOfStudents = _context.Employees.ToList();
+                return Ok(listOfStudents);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]EmployeeModel EmployeeObject)
+        {
+            using (var _context = new EmployeeDBContext())
+            {
+                _context.Employees.Add(EmployeeObject);
+                _context.SaveChanges();
+            }
+            return Ok("Student Added");
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody]EmployeeModel StudentObject)
+        {
+            using (var _context = new EmployeeDBContext())
+            {
+                _context.Employees.Update(StudentObject);
+                _context.SaveChanges();
+            }
+            return Ok("Student updated");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody]EmployeeModel StudentObject)
+        {
+            using (var _context = new EmployeeDBContext())
+            {
+                _context.Employees.Remove(StudentObject);
+                _context.SaveChanges();
+            }
+            return Ok("Student Deleted");
+        }
+
+    }
+}
---
+++ Data/EmployeeDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Employee_UsingEntityFrameWork.Models;
+
+namespace Employee_UsingEntityFrameWork.Data
+{
+    public class EmployeeDBContext:DbContext
+    {
+        public DbSet<EmployeeModel> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+              optionsBuilder.UseSqlServer(" Server=RYAMALA01; Database=Employee; Trusted_Connection=True; MultipleActiveResultSets=true; ");
+              optionsBuilder.UseSqlServer(" Server=RYAMALA01; Database=Employeeetails; Trusted_Connection=True; MultipleActiveResultSets=true; ");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
+
